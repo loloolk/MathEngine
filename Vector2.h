7658a2bb0fdf2e7e5c024c7a1f855cffa014bb9e
loloolk <marc.bondoger@gmail.com>
2022-03-15 15:08:54 -0600

    test

--- Vector2.h
+++ Vector2.h
@@ -29,23 +29,6 @@ namespace mtn {
 
             void absolute();
     };
-
-    class line {
-        public:
-            Vector2 start;
-            Vector2 end;
-
-            line();
-            line(Vector2 s, Vector2 e);
-            line(const line& l);
-            ~line();
-
-            line& operator = (const line& l);
-            void operator += (const Vector2& v);
-            line operator + (const Vector2& v)const;
-            void operator -= (const Vector2& v);
-            line operator - (const Vector2& v)const;
-    };
 }
 
 #endif
